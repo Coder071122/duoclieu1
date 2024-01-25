e5a739102e27c021991bce7984d8ddc389132846
QuocHung <dauhuyen1@gmail.com>
2024-01-26 01:56:09 +0700

    ds

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 new Vue({
     el: '#app',
     data: {
-        questions: [],  // Danh sách câu hỏi
+        questions: [],  
         currentQuestion: 1,
         userAnswers: Array(12).fill(''),
     },
     created() {
-        // Tạo danh sách câu hỏi ngẫu nhiên khi component được tạo
         this.questions = this.shuffleQuestions();
     },
     methods: {
         shuffleQuestions() {
-            // Lấy ngẫu nhiên 12 câu hỏi từ danh sách có sẵn
             const allQuestions = Array.from({ length: 39 }, (_, index) => index + 1);
             const shuffledQuestions = this.shuffleArray(allQuestions);
             return shuffledQuestions.slice(0, 12);
         },
         shuffleArray(array) {
-            // Hàm trộn mảng
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [array[i], array[j]] = [array[j], array[i]];
@@ -32,8 +29,6 @@ new Vue({
             }
 
             if (this.currentQuestion > 12) {
-                // Hiển thị kết quả
-                // Bạn có thể thực hiện xử lý khác ở đây tùy thuộc vào yêu cầu cụ thể
             }
         },
     },
