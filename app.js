2bfa541f216e0a0ee7a7ad7e5d199cfac6780c14
QuocHung <dauhuyen1@gmail.com>
2024-01-26 01:28:51 +0700

    done

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 new Vue({
     el: '#app',
     data: {
+        questions: [],  // Danh sách câu hỏi
         currentQuestion: 1,
         userAnswers: Array(12).fill(''),
     },
+    created() {
+        // Tạo danh sách câu hỏi ngẫu nhiên khi component được tạo
+        this.questions = this.shuffleQuestions();
+    },
     methods: {
+        shuffleQuestions() {
+            // Lấy ngẫu nhiên 12 câu hỏi từ danh sách có sẵn
+            const allQuestions = Array.from({ length: 39 }, (_, index) => index + 1);
+            const shuffledQuestions = this.shuffleArray(allQuestions);
+            return shuffledQuestions.slice(0, 12);
+        },
+        shuffleArray(array) {
+            // Hàm trộn mảng
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        },
         nextQuestion() {
             if (this.userAnswers[this.currentQuestion - 1] !== '') {
                 this.currentQuestion++;
